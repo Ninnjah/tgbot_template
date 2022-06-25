3052483c2acd9573d82ea6d1b8643ad3c9e5ba79
Ninnjah <yand.man4@gmail.com>
2022-06-25 19:00:29 +0300

    Remove lang parameter in add user method Remove unnecessary methods

--- tgbot/services/repository.py
+++ tgbot/services/repository.py
@@ -22,8 +22,7 @@ class Repo:
         firstname: str,
         fullname: str,
         lastname: Optional[str],
-        username: Optional[str],
-        lang: Optional[str] = None
+        username: Optional[str]
     ) -> None:
         """Store user in DB, ignore duplicates
 
@@ -35,10 +34,8 @@ class Repo:
         :type fullname: str
         :param lastname: User's lastname
         :type lastname: Optional[str]
-        :param lastname: User's username
-        :type lastname: Optional[str]
-        :param lang: Language str in ISO 639-1 standart
-        :type lang: Optional[str]
+        :param username: User's username
+        :type username: Optional[str]
         """
         # Create insert statement
         stmt = insert(users).values(
@@ -46,8 +43,7 @@ class Repo:
             firstname=firstname,
             fullname=fullname,
             lastname=lastname,
-            username=username,
-            lang=lang
+            username=username
         ).on_conflict_do_nothing()
 
         # Execute statement
@@ -88,27 +84,6 @@ class Repo:
         res = await self.conn.execute(stmt)
         # Return all found data in list of dicts or None
         return res.mappings().all()
-
-    async def update_user_lang(self, user_id: int, lang: str) -> None:
-        """Updates user language
-
-        :param user_id: User's telegram id
-        :type user_id: int
-        :param lang: Language str in ISO 639-1 standart
-        :type lang: str
-        """
-        # Create statement
-        stmt = update(users).values(
-            lang=lang
-        ).where(
-            users.c.user_id == user_id
-        )
-
-        # Execute statement
-        await self.conn.execute(stmt)
-        # Save changes
-        await self.conn.commit()
-        return
 
     # admins
     async def add_admin(self, user_id: int) -> None:
@@ -181,87 +156,3 @@ class Repo:
         res = await self.conn.execute(stmt)
         # Return all found data in list of dicts or None
         return res.mappings().all()
-    
-    # projects
-    async def add_project(self, owner_id: int, name: str, description: str) -> None:
-        """Stores project info DB, ignore duplicates
-
-        :param owner_id: Project owner telegram id
-        :type owner_id: int
-        :param name: Project name
-        :type name: str
-        :param description: Project description
-        :type description: str
-        """
-        # Create statement
-        stmt = insert(projects).values(
-            owner_id=owner_id,
-            name=name,
-            description=description
-        ).on_conflict_do_nothing()
-
-        # Execute statement
-        await self.conn.execute(stmt)
-        # Commit changes
-        await self.conn.commit()
-        return
-    
-    async def get_project_by_id(self, project_id: int) -> Optional[RowMapping]:
-        """Returns project from DB by project id
-
-        :param project_id: Project id
-        :type project_id: int
-        :return: Project info
-        :rtype: list
-        """
-        # Create statement
-        stmt = select(projects).where(
-            projects.c.project_id == project_id
-        )
-        # Execute statement
-        res = await self.conn.execute(stmt)
-        try:
-            # Try to return one result
-            return res.mappings().one()
-
-        except NoResultFound:
-            # If no results found return None
-            return None
-    
-    async def get_users_projects(self, owner_id: int) -> List[RowMapping]:
-        """Returns all users project from DB by owner_id
-
-        :param owner_id: Project owner telegram id
-        :type owner_id: int
-        :return: Projects list
-        :rtype: list
-        """
-        # Create statement
-        stmt = select(projects).where(
-            projects.c.owner_id == owner_id
-        )
-        
-        # Execute statement
-        res = await self.conn.execute(stmt)
-        # Return all results
-        return res.mappings().all()
-
-    async def del_project_by_id(self, project_id: int) -> int:
-        """Remove project from DB by project id
-
-        :param project_id: Project id
-        :type project_id: int
-        :return: Removed row count
-        :rtype: int
-        """
-        # Create statement
-        stmt = delete(projects).where(
-            projects.c.project_id == project_id
-        )
-
-        # Execute statement
-        res = await self.conn.execute(stmt)
-        # Save changes
-        await self.conn.commit()
-        # Return deleted row count
-        return res.rowcount
